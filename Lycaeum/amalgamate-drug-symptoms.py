d77bb715fe9e05e9564e5eb2f39bf7e980fac193
Michael Chary <mac389@gmail.com>
2019-08-26 07:56:52 -0400

    Update amalgamate-drug-symptoms.py

--- Lycaeum/amalgamate-drug-symptoms.py
+++ Lycaeum/amalgamate-drug-symptoms.py
@@ -11,13 +11,25 @@ drugs_symptoms = json.load(open(os.path.join('.','Lycaeum','drugs_symptoms.json'
 
 #group symptoms
 
+def flatten(alist):
+	ans = []
+	for item in alist:
+		if isinstance(item,str):
+			ans += [item]
+		elif isinstance(item,dict):
+			ans += item.values()
+	return list(itertools.chain.from_iterable(ans))
+
+
 classes = ontology.keys()
 drugs = drugs_symptoms.keys()
 
-arr = np.zeros((drugs,classes))
+arr = np.zeros((len(drugs),len(classes)))
 
-class_symptoms = {classe: for classe in classes}
-
+class_symptoms = {key:flatten(value) for key,value in ontology.iteritems()}
+ap(class_symptoms)
+'''
 for i, drug in enumerate(tqdm(drugs,'Drugs')):
 	for j,classe in enumerate(tqdm(classes,'Class')):
 		arr[i,j] =  lclasses[classe] drugs_symptoms[drugs]
+'''
