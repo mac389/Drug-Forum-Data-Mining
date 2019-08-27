1efc7fc167ba1f53da6d437f4b0baf35949b5fde
Michael Chary <mac389@gmail.com>
2019-08-27 07:32:56 -0400

    Update

--- Lycaeum/amalgamate-drug-symptoms.py
+++ Lycaeum/amalgamate-drug-symptoms.py
@@ -3,22 +3,23 @@ import json, os, itertools
 import pandas as pd 
 import numpy as np 
 
-from awesome_print import ap 
+#from awesomeprint import ap 
 from tqdm import tqdm 
+from collections import Counter
 
-ontology = json.load(open(os.path.join('..','classification-standardized-symptoms.json'),'r'))
-drugs_symptoms = json.load(open(os.path.join('.','Lycaeum','drugs_symptoms.json'),'r'))
+ontology = json.load(open(os.path.join('.','ontology.json'),'r'))
+drugs_symptoms = json.load(open(os.path.join('.','drugs_symptoms.json'),'r'))
 
 #group symptoms
 
 def flatten(alist):
-	ans = []
-	for item in alist:
-		if isinstance(item,str):
-			ans += [item]
-		elif isinstance(item,dict):
-			ans += item.values()
-	return list(itertools.chain.from_iterable(ans))
+  ans = []
+  for item in alist:
+    if isinstance(item,str) or isinstance(item,unicode):
+      ans += [item]
+    elif isinstance(item,dict):
+      ans += item.values()[0]
+  return ans
 
 
 classes = ontology.keys()
@@ -26,10 +27,13 @@ drugs = drugs_symptoms.keys()
 
 arr = np.zeros((len(drugs),len(classes)))
 
-class_symptoms = {key:flatten(value) for key,value in ontology.iteritems()}
-ap(class_symptoms)
-'''
+class_symptoms = {key:set(flatten(value)) for key,value in ontology.iteritems()}
+#ap(class_symptoms)
+
+
 for i, drug in enumerate(tqdm(drugs,'Drugs')):
 	for j,classe in enumerate(tqdm(classes,'Class')):
-		arr[i,j] =  lclasses[classe] drugs_symptoms[drugs]
-'''
+		arr[i,j] =  len(class_symptoms[classe] & set(drugs_symptoms[drug]))
+
+df = pd.DataFrame(arr,columns = classes, index=drugs)
+df.to_csv('./frequency-of-symptoms.csv')
