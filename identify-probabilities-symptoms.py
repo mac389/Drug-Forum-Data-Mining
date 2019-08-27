628fdb041019577a82ade162a1686382bb452f5c
Michael Chary <mac389@gmail.com>
2019-08-27 14:25:27 -0400

    Reorganizing files

--- identify-probabilities-symptoms.py
+++
@@ -1,5 +0,0 @@
-from awesome_print import  ap 
-
-symptoms = open('./putative-standarized-symptoms.txt','r')
-
-ap(symptoms)
--- identify-probabilities-symptoms.rb
+++
@@ -1,6 +0,0 @@
-require 'awesome_print'
-
-f = File.open('putative-standardized-symptoms.txt') or die 'Could not find file'
-
-symptoms = f.each_line{|line|}
-ap symptoms
