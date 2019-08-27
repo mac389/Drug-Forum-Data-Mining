3467738204ba7592e1b107ec8993a21c7e24005f
Michael Chary <mac389@gmail.com>
2019-08-27 14:45:04 -0400

    Cookie cutting

--- src/analysis/drug_name_frequencies.py
+++ src/analysis/drug_name_frequencies.py
@@ -1,12 +1,18 @@
-from collections import Counter
+import json 
 
 import matplotlib.pyplot as plt 
 import Graphics as artist 
+import numpy as np 
 
 from matplotlib import rcParams
+from collections import Counter
+
 rcParams['text.usetex'] = True
 
-drug_names = open('drug_names','r').read().splitlines()
+drug_names = json.load(open('lycaeum-forum-processed-has-drug-names.json','r'))
+filtered_list = open('deduplicated-curated-drug-names').read().splitlines()
+
+drug_names = [drug for item in drug_names.itervalues() for drug in item['drugs'] if drug in filtered_list]
 drug_name_frequencies = Counter(drug_names)
 
 cutoff=20
@@ -19,7 +25,7 @@ ax.bar(idx,frequency, color='k')
 
 artist.adjust_spines(ax)
 ax.set_ylabel(artist.format('No. of mentions'))
-ax.set_xticks(range(len(name)))
+ax.set_xticks(np.array(range(len(name)))+0.4)
 ax.set_xticklabels(map(artist.format,name), rotation=90)
 
 a = plt.axes([.65, .6, .2, .2])
@@ -32,4 +38,4 @@ plt.yscale('log', nonposy='clip')
 for label in a.get_xticklabels()[1::2]:
     label.set_visible(False)
 plt.tight_layout()
-plt.savefig('drug-name-frequency.png')
+plt.savefig('drug-name-frequency-lycaeum.png')
