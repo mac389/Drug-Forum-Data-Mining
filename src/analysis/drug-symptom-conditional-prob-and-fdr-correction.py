96b44e3e5083695f03ff1ec4400094bb19a1fdd1
Michael Chary <mac389@gmail.com>
2019-10-05 13:17:40 -0400

    update

--- src/analysis/drug-symptom-conditional-prob-and-fdr-correction.py
+++ src/analysis/drug-symptom-conditional-prob-and-fdr-correction.py
@@ -2,9 +2,13 @@ import json, os, itertools
 
 import pandas as pd
 import numpy as np 
+import matplotlib.pyplot as plt
 
 from statsmodels.stats.multitest import fdrcorrection
-from awesome_print import ap 
+from awesome_print import ap
+from scipy.stats import percentileofscore 
+from tqdm import tqdm 
+
 df = pd.read_csv(os.path.join('..','..','data','processed','drug-symptom-frequency.csv'),index_col=0)
 
 '''
@@ -22,9 +26,6 @@ p(drug) => sum over symptoms [not perfect, overcounts]
 
 baseline_probabilities = df.sum(axis=1)/(df.sum(axis=1).sum())
 overall_sum = df.sum().sum()
-
-
-baseline_probabilities[baseline_probabilities==0]=1
 
 '''
 Create conditional probability DataFrame, formatted as p(effect | drug)
@@ -52,11 +53,29 @@ data = [("%s|%s"%(row,col),conditional_prob_df.loc[row,col])
 								  for col in conditional_prob_df.columns
 								  if row != col]
 
+
 long_cdf = pd.DataFrame(data,columns=['name','conditional_probability'])
 long_cdf = long_cdf[(~long_cdf['conditional_probability'].isna()) & (long_cdf['conditional_probability']!=0)]
 long_cdf['percentile'] = long_cdf['conditional_probability'].rank(pct=True)
 
-reject, adjusted_p_value = fdrcorrection(long_cdf['conditional_probability'], alpha=0.05)
+n = 20
+x = []
+for _ in tqdm(range(n),'bootstrapping'):
+	x += [long_cdf.sample(frac=1).as_matrix(columns=['conditional_probability']).ravel()]
+
+dist = pd.DataFrame(list(itertools.chain.from_iterable(x)))
+dist.dropna(inplace=True)
+
+
+long_cdf['p_value'] = long_cdf['conditional_probability'].apply(lambda x: percentileofscore(x,dist.to_numpy()))
+'''
+dist.hist(bins=100)
+plt.show()
+'''
+#build distribution
+
+
+reject, adjusted_p_value = fdrcorrection(long_cdf['p_value'], alpha=0.05)
 long_cdf['adjusted_p_value'] = adjusted_p_value
 long_cdf['should accept'] = reject
 long_cdf.to_csv(os.path.join('..','..','data','interim','significant_drug_symptom_combinations_after_bh.csv'))
