0f099af8a73ad334879883a0bd8b4752d1eb7f4a
Michael Chary <mac389@gmail.com>
2019-12-06 16:09:57 -0500

    Update calculate-overall-p-values.py

--- src/analysis/calculate-overall-p-values.py
+++ src/analysis/calculate-overall-p-values.py
@@ -4,6 +4,8 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 
+from scipy.stats import rankdata
+
 DATA_PATH = os.path.join('..','..','data','processed')
 
 filenames = ['symptom-symptom-frequency', 
@@ -21,17 +23,15 @@ fdr = 0.05
 df = pd.concat(dfs)
 df.drop(df.columns[0],axis=1,inplace=True)
 #df['p_value'] /= 100.
+
 df.sort_values(by='p_value',inplace=True, ascending=True)
 df['rank'] = df['p_value'].rank()
-df['bh_threshold'] = df['rank']/len(df['rank'])*fdr
-#sns.scatterplot(data=df['p_value'])
-#plt.show()
-
-'''
-reject, adjusted_p_value = fdrcorrection(df['p_value'], method='negcorr', alpha=0.05)
-df['adjusted_p_value'] = adjusted_p_value
-df['should reject'] = reject
-'''
-print(df.head(14)) #everything is significant because the number of statements are so large. 
-#print df['should reject'].describe()
+print('Calculating CM')
+cm = sum(1/i for i in range(1,len(df['rank'])))
+print('Calculated')
+df['bh_threshold'] = df['rank']/(len(df['rank'])*cm)*fdr
+df['should accept'] = df['p_value']<df['bh_threshold']
+print(df.head(14)) 
+print(df['should accept'].describe())
+#nothing is significant because the number of statements are so large. 
 #df.to_csv(os.path.join(DATA_PATH,'knowledge-base-as-df.csv'),index=False)
